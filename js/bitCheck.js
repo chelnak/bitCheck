1f40bf0345eb4fab688704b1a0083eed5cb47cb1
Craig Gumbley <craiggumbley@gmail.com>
2014-08-21 16:07:48 +0100

    add getJSON function

--- js/bitCheck.js
+++ js/bitCheck.js
@@ -1,21 +1,68 @@
- 
 var queryBlockChain = function(e) {
 
-var blockChainUrl = " https://blockchain.info/address/";
+    var blockChainUrl = " https://blockchain.info/address/";
+    
+    if (e.selectionText) {
+			blockChainUrl += e.selectionText;	
+		}
 
-if (e.selectionText) {
-	blockChainUrl += e.selectionText;	
-	}
+  
+   chrome.tabs.create({"url" : blockChainUrl });
 
-   chrome.tabs.create(
-          {"url" : blockChainUrl });
 
 }
+
+ 
+var queryBlockChainJSON = function(e) {
+
+    var blockChainUrl = " https://blockchain.info/address/";
+    
+    if (e.selectionText) {
+			blockChainUrl += e.selectionText += "?format=json";	
+		}
+
   
- chrome.contextMenus.create({
+	var test =getJSON(blockChainUrl);
+	
+	//return a random value here to test 	
+ 	alert(test['hash160']);
+
+}
+
+
+function getJSON(blockChainUrl) {
+	
+	var resp;	
+	
+   var req = new XMLHttpRequest();
+    
+   req.open('GET', blockChainUrl, false);
+   
+   req.onreadystatechange = function () {
+
+   if (req.readyState == 4){
+			resp = JSON.parse(req.responseText);
+			}      
+		}
+    
+   req.send();
+    
+   return resp;
+	
+}
+
+
+chrome.contextMenus.create({
     "title" : "Check BTC Address",
     "contexts" : ["selection"],
 	 "onclick" : queryBlockChain
-  });
+});
+
+chrome.contextMenus.create({
+    "title" : "JSON Check BTC Address",
+    "contexts" : ["selection"],
+	 "onclick" : queryBlockChainJSON
+});
+
   
   
